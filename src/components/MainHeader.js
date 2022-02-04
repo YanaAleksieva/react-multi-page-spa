c12dbc155361e6d5426557194031dbc7aedc8b1a
YanaAleksieva <ya.aleksieva@gmail.com>
2022-02-04 10:48:34 +0200

    using NavLinks

--- src/components/MainHeader.js
+++ src/components/MainHeader.js
@@ -1,17 +1,23 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
+
+import classes from "./MainHeader.module.css";
 
 const MainHeader = () => {
   return (
-    <header>
+    <header className={classes.header}>
       <nav>
         <ul>
           <li>
             {/* <a href="/welcome">... - if we use this we are making a new request every time when clicked, and this is not the goal because of we use states - we will lose our info and we won't be reactive
              */}
-            <Link to="/welcome">Welcome</Link>
+            <NavLink activeClassName={classes.active} to="/welcome">
+              Welcome
+            </NavLink>
           </li>
           <li>
-            <Link to="/products">Products</Link>
+            <NavLink activeClassName={classes.active} to="/products">
+              Products
+            </NavLink>
           </li>
         </ul>
       </nav>
