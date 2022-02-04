a353f1fde6d22a35ef46954b91956c0ac27250b7
YanaAleksieva <ya.aleksieva@gmail.com>
2022-02-04 09:59:35 +0200

    basic - define and use routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
+import { Route } from "react-router-dom";
+
+import Welcome from "./pages/Welcome";
+import Products from "./pages/Products";
+
 function App() {
   return (
     <div>
-      <h2>Let's get started!</h2>
+      {
+        // Route component will make sure that Welcome component is displayed only if our URL path is /welcome
+      }
+      <Route path="/welcome">
+        <Welcome />
+      </Route>
+      <Route path="/products">
+        <Products />
+      </Route>
     </div>
   );
 }
